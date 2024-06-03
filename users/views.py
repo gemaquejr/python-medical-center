4ea5c289ef68fb30fea177c876908fc6706f1b2f
gemaquejr <gemaquejr@hotmail.com>
2024-06-03 17:39:16 -0300

    Diferenciando o tipo de requisição para o formulario em register.html
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- users/views.py
+++ users/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
-    return render(request, "register.html")
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        return HttpResponse('Teste')
