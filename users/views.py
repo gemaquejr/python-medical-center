e1866ceb85d2169d1cbdf6d567c5769cd759bd6b
gemaquejr <gemaquejr@hotmail.com>
2024-06-03 18:03:53 -0300

    Adiciona validação de senhas
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
@@ -7,4 +7,17 @@ def register(request):
     if request.method == "GET":
         return render(request, 'register.html')
     elif request.method == "POST":
-        return HttpResponse('Teste')
+        usernane = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            print('Senhas não conferem')
+            return redirect('register')
+
+        if len(senha) < 6:
+            print('Senha muito curta')
+            return redirect('register')
+
+        return HttpResponse('Usuário cadastrado com sucesso')
