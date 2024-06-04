c33e531cc4e2e2fa3ec3d4997ca93be5596d6399
gemaquejr <gemaquejr@hotmail.com>
2024-06-03 21:22:30 -0300

    Autenticando o usuario na página login.html
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 
 # Create your views here.
@@ -42,3 +43,15 @@ def register(request):
 def login_view(request):
     if request.method == "GET":
         return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/patients/home')
+
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+        return redirect('/users/login')
