787240d305580c3c3dc69a0da6f51642d0a13b5b
gemaquejr <gemaquejr@hotmail.com>
2024-06-04 20:54:14 -0300

    Criando a base da funcionalidade de patients
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- healing/urls.py
+++ healing/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("doctors/", include("doctors.urls")),
+    path("patients/", include("patients.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
