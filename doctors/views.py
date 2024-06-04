9a8305b1e6272646926f1e850692770093593d8b
gemaquejr <gemaquejr@hotmail.com>
2024-06-03 23:51:54 -0300

    Mostrando o nome da especialidade do médico na página register_doctor.html
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- doctors/views.py
+++ doctors/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Especialidades
 
 
 # Create your views here.
 def register_doctor(request):
     if request.method == 'GET':
-        return render(request, 'register_doctor.html')
+        especialidades = Especialidades.objects.all()
+        return render(request, 'register_doctor.html', {'especialidades': especialidades})
