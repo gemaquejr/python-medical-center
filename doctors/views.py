0bc7a476e18cc0fbb23e1b27ea2cc43fc446d21d
gemaquejr <gemaquejr@hotmail.com>
2024-06-04 00:20:56 -0300

    Salvando em banco de dados as informações do formulario em register_doctor.html
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- doctors/views.py
+++ doctors/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Especialidades
+from .models import Especialidades, DadosMedico
 
 
 # Create your views here.
@@ -7,3 +7,33 @@ def register_doctor(request):
     if request.method == 'GET':
         especialidades = Especialidades.objects.all()
         return render(request, 'register_doctor.html', {'especialidades': especialidades})
+    elif request.method == 'POST':
+        crm = request.POST.get('crm')
+        nome = request.POST.get('nome')
+        cep = request.POST.get('cep')
+        endereco = request.POST.get('rua')
+        bairro = request.POST.get('bairro')
+        numero = request.POST.get('numero')
+        cim = request.FILES.get('cim')
+        rg = request.FILES.get('rg')
+        foto = request.FILES.get('foto')
+        especialidade = request.POST.get('especialidade')
+        descricao = request.POST.get('descricao')
+        valor_consulta = request.POST.get('valor_consulta')
+
+        dados_medico = DadosMedico(
+            crm=crm,
+            nome=nome,
+            cep=cep,
+            endereco=endereco,
+            bairro=bairro,
+            numero=numero,
+            rg=rg,
+            cedula_identidade_medica=cim,
+            foto=foto,
+            especialidade_id=especialidade,
+            descricao=descricao,
+            valor_consulta=valor_consulta
+        )
+
+        dados_medico.save()
