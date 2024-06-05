1700ef4473535a96b2e79d2ad305abc5b8b65015
gemaquejr <gemaquejr@hotmail.com>
2024-06-05 18:01:11 -0300

    Preenchendo dinâmicamente a foto, nome e descriçãp na página home.html em patients
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from doctors.models import DadosMedico
 
 
 # Create your views here.
 def home(request):
     if request.method == "GET":
-        return render(request, 'home.html')
+        medicos = DadosMedico.objects.all()
+        return render(request, 'home.html', {'medicos': medicos})
