d7a920caac519dc8cb4ad62d9960ac4e4fb0f354
gemaquejr <gemaquejr@hotmail.com>
2024-06-05 19:25:00 -0300

    Aplicando filtro em nome e especialidades nos medicos na pagina home.html
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- patients/views.py
+++ patients/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from doctors.models import DadosMedico
+from doctors.models import DadosMedico, Especialidades
 
 
 # Create your views here.
 def home(request):
     if request.method == "GET":
+        medico_filtrar = request.GET.get('medico')
+        especialidades_filtrar = request.GET.getlist('especialidades')
         medicos = DadosMedico.objects.all()
-        return render(request, 'home.html', {'medicos': medicos})
+
+        if medico_filtrar:
+            medicos = medicos.filter(nome__icontains=medico_filtrar)
+
+        if especialidades_filtrar:
+            medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+
+        especialidades = Especialidades.objects.all()
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
